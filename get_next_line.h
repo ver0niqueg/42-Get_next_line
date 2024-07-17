f4bbf47b00c193557938da07879a1c250cb1caa5
Veronique Galmiche <vgalmich@made-f0Ar8s8.clusters.42paris.fr>
2024-07-17 20:35:57 +0200

    utils added

--- get_next_line.h
+++ get_next_line.h
@@ -6,7 +6,7 @@
 /*   By: vgalmich <vgalmich@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/07/05 12:53:32 by vgalmich          #+#    #+#             */
-/*   Updated: 2024/07/16 14:36:18 by vgalmich         ###   ########.fr       */
+/*   Updated: 2024/07/17 20:35:20 by vgalmich         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,3 +16,9 @@
 #ifndef BUFFER_SIZE
 # define BUFFER_SIZE 10
 # endif
+
+size_t	ft_strlen(const char *str)
+char	*ft_strchr(const char *s, int c);
+char	*ft_strdup(const char *src)
+char	*ft_substr(const char *s, unsigned int start, size_t len)
+char	*ft_strjoin(char const *s1, char const *s2)
--- get_next_line_utils.c
+++ get_next_line_utils.c
@@ -6,7 +6,7 @@
 /*   By: vgalmich <vgalmich@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/07/16 14:36:02 by vgalmich          #+#    #+#             */
-/*   Updated: 2024/07/16 14:36:05 by vgalmich         ###   ########.fr       */
+/*   Updated: 2024/07/17 20:35:23 by vgalmich         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,3 +17,96 @@ ft_substr : pour extraire une sous chaine de stash a line
 ft_strjoin : pour concatener 2 chaines (tash + line)
 ft_strlen : pour calculer la longueur d'une chaine
 */
+
+ft_calloc
+
+size_t	ft_strlen(const char *str)
+{
+	size_t	i;
+
+	i = 0
+	while (str[i])
+	{
+		i++;
+	}
+	return (i);
+}
+
+char	*ft_strchr(const char *s, int c)
+{
+	unsigned int	i;
+
+	i = 0;
+	while (s[i])
+	{
+		if (s[i] == (char)c)
+			return ((char *)s);
+		s++;
+	}
+	if (s[i] == (char)c)
+		return ((char *)s);
+	return (NULL);
+}
+
+char	*ft_strdup(const char *src)
+{
+	char	*dest;
+	int		i;
+	int		len;
+
+	i = 0;
+	len = ft_strlen(src);
+	dest = (char *)malloc(sizeof(char) * (len + 1));
+	if (!dest)
+		return (NULL);
+	while (i < len)
+	{
+		dest[i] = src[i];
+		i++;
+	}
+	dest[i] = '\0';
+	return (dest);
+}
+
+char	*ft_substr(const char *s, unsigned int start, size_t len)
+{
+	size_t	i;
+	char	*str;
+
+	if (!s)
+		return (NULL);
+	if (start > ft_strlen(s))
+		return (ft_strdup(""));
+	if (len > ft_strlen(s + start))
+		len = ft_strlen(s + start);
+	str = ft_calloc(len + 1, sizeof(char));
+	if (!str)
+		return (NULL);
+	while (i < len)
+	{
+		str[i] = s[start + i];
+		i++;
+	}
+	return (str);
+}
+
+char	*ft_strjoin(char const *s1, char const *s2)
+{
+	int		i;
+	int		j;
+	char	*str;
+
+	i = 0;
+	j = 0;
+	str = (char *)malloc((ft_strlen(s1) + ft_strlen(s2) + 1) * sizeof(char));
+	if (!str)
+		return (NULL);
+	i = 0;
+	while (s1[i])
+		str[j++] = s1[i++];
+	i = 0;
+	while (s2[i])
+		str[j++] = s2[i++];
+	str[j] = '\0';
+	return (str);
+}
